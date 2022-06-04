a7b83333e38a3d4dd48ebf46ceadca1e8b8d8910
DominicSamoes <dominicsamo@outlook.com>
2022-06-04 09:19:10 +0200

    Python 0x03 Task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    leng = len(my_string)
+    a = 0
+    strng = my_string[:]
+    for b in range(leng):
+        if (my_string[b] == 'c' or my_string[b] == 'C'):
+            strng = strng[:(b - a)] + my_string[(b + 1):]
+            a += 1
+    return (strng)
