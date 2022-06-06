9cd19dc3d66ce1dfee3c99a0125125e76e752995
DominicSamoes <dominicsamo@outlook.com>
2022-06-06 20:33:07 +0200

    Python 0x03 Task 5 Corrections

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env/python3
+#!/usr/bin/python3
 def no_c(my_string):
-    leng = len(my_string)
-    a = 0
-    strng = my_string[:]
-    for b in range(leng):
-        if (my_string[b] == 'c' or my_string[b] == 'C'):
-            strng = strng[:(b - a)] + my_string[(b + 1):]
-            a += 1
-    return (strng)
+    kop = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(kop))
