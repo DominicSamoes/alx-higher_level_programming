f97ff158fe3c418a2f9dca9f10975122a7639138
DominicSamoes <dominicsamo@outlook.com>
2022-06-21 15:29:11 +0200

    0x06 Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Define a square"""
+
+    def __init__(self, size):
+        """Init a new square.
+
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
