1f33b4224c5a30a2e07ff4288b12e28e39523077
DominicSamoes <dominicsamo@gmail.com>
2022-07-04 22:37:17 +0200

    0x0A. Python - Inheritance Task 11 Correction

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
+"""Creates a Suare Class"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ Class defines a Square from Rectangle class """
+    """Class defines a Square from Rectangle class"""
     def __init__(self, size):
-        """ Method initialises a Square """
+        """Method initialises a Square"""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
 
     def area(self):
-        """ Method returns a string with the area """
+        """Method returns a string with the area"""
         return super().area()
 
     def __str__(self):
-        """ Special method returns a printable string """
+        """Special method returns a printable string"""
         return "[Square] {}/{}".format(self.__size, self.__size)
