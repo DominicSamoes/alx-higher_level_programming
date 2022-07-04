bb7ac4c303c657390db61fbf8b3b2b8fab385b2c
DominicSamoes <dominicsamo@gmail.com>
2022-07-04 22:47:11 +0200

    0x0A. Python - Inheritance Task 0 Correction

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Finds list of available attributes and methods"""
+
+
 def lookup(obj):
     """Function that returns the list of available attributes
 and methods of an object
