9a3f935549079474ca301d4a9ec0017f079828de
DominicSamoes <dominicsamo@gmail.com>
2022-07-04 22:53:44 +0200

    0x0A. Python - Inheritance Task 3 Correction

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Finds if an object is an instance of an inherited class"""
+
+
 def is_kind_of_class(obj, a_class):
-    """ Method that returns True/False if obj is an
+    """Method that returns True/False if obj is an
     instance of a_class
     Args:
         obj: object
