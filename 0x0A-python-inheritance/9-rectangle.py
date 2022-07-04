4a28d40efd1cc851d2c51fb00a70955c88703aab
DominicSamoes <dominicsamo@gmail.com>
2022-07-04 23:04:30 +0200

    0x0A. Python - Inheritance Task 8-9 Correction

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
+"""Class inherits from task based on 8-rectangle.py"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ Class defines a rectangle from BaseGeometry Class """
+    """Class defines a rectangle from BaseGeometry Class"""
 
     def __init__(self, width, height):
-        """ Initialises instance """
+        """Initialises instance"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
-        """ Method returns the area of the instance"""
+        """Method returns the area of the instance"""
         return self.__width * self.__height
 
     def __str__(self):
-        """ Special method returns the printable string """
+        """Special method returns the printable string"""
         return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
