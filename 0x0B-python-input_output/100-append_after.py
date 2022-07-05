312c57aec8be11ea240ac32810bf38281f4292f7
DominicSamoes <dominicsamo@gmail.com>
2022-07-05 20:12:16 +0200

    0x0B. Python - Input/Output Task 13 Correction

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""Method inserts a line of text to a file,
-after each line containing a specific string
-"""
+"""Executes a method that appends a line"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """Insert new_string after each line search_string
-    Args:
-        filename: name of text file
-        search_string: string to search for within the file
-        new_string: string to be inserted
-    """
-    with open(filename, "r") as readfile:
-        text = readfile.readlines()
+    """Method appends a new line when a string is found
 
-    with open(filename, "w") as outfile:
-        strng = ""
-        for line in text:
-            strng += line
-            if search_string in line:
-                strng += new_string
-            outfile.write(strng)
+    Args:
+        filename: filename
+        search_string: string to search
+        new_string: string to append
+    """
+
+    res_line = []
+    with open(filename, 'r', encoding="utf-8") as f:
+        for line in f:
+            res_line += [line]
+            if line.find(search_string) != -1:
+                res_line += [new_string]
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write("".join(res_line))
