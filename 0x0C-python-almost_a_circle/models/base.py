428a668cc552c8ff50dc59fd3b4ce6442784c0fc
DominicSamoes <dominicsamo@gmail.com>
2022-07-11 20:39:06 +0200

    0x0C Task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/python3
+"""First class"""
+
+
+class Base:
+    """Defines the base class.
+    Attributes:
+        __nb_objects: The number of instantiated bases
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Class constructor
+        Args:
+            id: Identityof Base
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
