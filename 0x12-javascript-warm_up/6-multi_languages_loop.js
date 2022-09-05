a6b0fb110162594c9ba347546d8f70df8d3ad9b3
DominicSamoes <dominicsamo@outlook.com>
2022-09-06 01:49:18 +0200

    0x12 Tasks 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const st = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const s = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < 3; i++) {
-  console.log(st[i]);
+  console.log(s[i]);
 }
