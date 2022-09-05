5d42ef6abe8eb3e7940536b037cb21a1f8a7f421
DominicSamoes <dominicsamo@outlook.com>
2022-09-06 01:32:22 +0200

    0x12 Tasks 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,11 +2,8 @@
 function factorial (n) {
   if (n === 0 || isNaN(n))
        return 1;
-  else if (n < 0)
-       return -1;
   else
-       return n * factorial(n - 1);
-      
+       return n * factorial(n - 1);  
 }
 
 console.log(factorial(Number(process.argv[2])));
