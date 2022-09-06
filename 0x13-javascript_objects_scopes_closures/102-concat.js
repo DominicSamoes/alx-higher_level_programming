3997859b4717a9b2e07bff3a0adc415480febdd3
DominicSamoes <dominicsamo@outlook.com>
2022-09-06 22:59:18 +0200

    0x13 Tasks 8-13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const fc = require('fc');
+const fc = require('fs');
 const a = fc.readFileSync(process.argv[2], 'utf8');
 const b = fc.readFileSync(process.argv[3], 'utf8');
 fc.writeFileSync(process.argv[4], a + b);
