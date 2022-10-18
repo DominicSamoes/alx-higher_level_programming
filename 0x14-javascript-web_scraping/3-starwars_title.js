581a77959615d9d25d95d8af67bc44077abc639a
DominicSamoes <dominicsamo@outlook.com>
2022-10-18 22:33:24 +0200

    0x14 Task 3 Correction

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2]; 
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
-	if (error) {
-		console.error(error);
-	} else {
-		console.log(JSON.parse(body).title);
-	}
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
