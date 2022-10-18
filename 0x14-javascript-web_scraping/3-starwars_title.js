1b53fa1ca18e72c38d65b8370037dd7ace11909c
DominicSamoes <dominicsamo@outlook.com>
2022-10-18 21:51:50 +0200

    0x14 Tasks

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], 
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/', 
 function (error, response, body) {
-	if (error) {
-		console.error(error);
+	if (error == null) {
+		const json = JSON.parse(body);
+		console.log(json.title);
 	}
-  console.log(JSON.parse(body).title);
 });
