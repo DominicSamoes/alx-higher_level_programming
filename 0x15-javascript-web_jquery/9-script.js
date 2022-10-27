c65630871f9e363081f9fc198f25600dadb969b0
DominicSamoes <dominicsamo@outlook.com>
2022-10-27 15:28:10 +0200

    0x15. JavaScript - Web jQuery Tasks

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,5 +1,5 @@
 $('document').ready(function () {
-  $.get('https://stefanbohacek.com/hellosalut/?lang=es', function (data) {
+  $.get('https://stefanbohacek.com/hellosalut/?lang=fr', function (data) {
     $('DIV#hello').text(data.hello);
   });
 });
