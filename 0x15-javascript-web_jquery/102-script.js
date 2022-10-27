aef8ba0a835f0530fd93808400fb955d9acb6fa1
DominicSamoes <dominicsamo@outlook.com>
2022-10-27 14:51:22 +0200

    0x15. JavaScript - Web jQuery Tasks

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,5 +1,5 @@
 $('document').ready(function () {
-  const url = 'https://www.fourtonfish.com/hellosalut/?';
+  const url = 'https://stefanbohacek.com/hellosalut/?';
   $('INPUT#btn_translate').click(function () {
     $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
       $('DIV#hello').html(data.hello);
